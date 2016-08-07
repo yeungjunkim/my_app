cd0d032f48af1ac66519c2aa597e0684c3db08b6
kimyj <pooh97@gmail.com>
2016-08-07 22:25:38 +0900

    express study

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
+app.use(express.static(__dirname + '/public'));
+console.log(__dirname);
 app.listen(3000, function(){
   console.log('Server On!');
 });
