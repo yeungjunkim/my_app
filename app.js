625f7698ee85fe4804f3369269bd9469e4630688
kimyj <pooh97@gmail.com>
2016-08-07 23:38:03 +0900

    db connect

--- app.js
+++ app.js
@@ -1,9 +1,78 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
+var mongoose = require('mongoose');
 
-app.use(express.static(__dirname + '/public'));
-console.log(__dirname);
-app.listen(3000, function(){
-  console.log('Server On!');
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+
+db.once("open", function () {
+  console.log("DB connected!");
+});
+db.on("error", function (err) {
+  console.log("DB ERROR :", err);
+});
+
+var dataschema = mongoose.Schema({
+  name:String,
+  count:Number
+});
+var Data = mongoose.model('data', dataschema);
+Data.findOne({name:"myData"}, function(err,data){
+  if(err) return console.log("Data ERROR:", err);
+  if(!data){
+    Data.create({name:"myData", count:0}, function (err, data) {
+    if(err) return console.log("Data ERROR:", err);
+    console.log("Counter initialized :", data);
+  });
+  }
+});
+app.set("view engine", 'ejs');
+app.use(express.static(path.join(__dirname, '/public')));
+
+var data={count:0};
+app.get('/', function (req, res) {
+  Data.findOne({name:"myData"}, function(err,data) {
+    if(err) return console.log("Data ERROR:", err);
+    data.count++;
+    data.save(function (err) {
+      if(err) return console.log("Data ERROR:", err);
+      res.render("my_first_ejs", data);
+    });
+  });
+});
+app.get('/reset', function (req, res) {
+  setCounter(res, 0);
+});
+app.get('/set/count', function (req, res) {
+  if(req.query.count) setCounter(res, req.query.count);
+  else getCounter(res);
+});
+app.get('/set/:num', function (req, res) {
+  if(req.params.count) setCounter(res, req.params.count);
+  else getCounter(res);
+});
+
+function setCounter(res,num) {
+  console.log("setCounter");
+  Data.findOne({name:"myData"}, function(err,data) {
+    if(err) return console.log("Data ERROR:", err);
+    data.count=num;
+    data.save(function (err) {
+      if(err) return console.log("Data ERROR:", err);
+      res.render("my_first_ejs", data);
+    });
+  });
+}
+
+function getCounter(res,num) {
+  console.log("getCounter");
+  Data.findOne({name:"myData"}, function(err,data) {
+    if(err) return console.log("Data ERROR:", err);
+    res.render("my_first_ejs", data);
+  });
+}
+
+app.listen(3000, function() {
+console.log('Server On!');
 });
