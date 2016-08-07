04c595c689ae43292c13e50a5a6514c95a4022f1
kimyj <pooh97@gmail.com>
2016-08-07 22:15:22 +0900

    express add

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-function fizzBuzz() {
-  for(i=1;i<=100;i++)
-  {
-    if(i%3 === 0 && i%5 === 0) {
-      console.log("FizzBuzz");
-    }
-    else if(i%3 === 0) {
-      console.log("Fizz");
-    }
-    else if(i%3 === 0) {
-      console.log("Buzz");
-    }
-    else {
-      console.log("i");
-    }
-  }
-}
-fizzBuzz();
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function(){
+  console.log('Server On!');
+});
